155240d7e537d5048ec1006dd2a9d3b239f0c8be
AlanisYates <yatesalanis@gmail.com>
2020-05-26 23:47:23 -0400

    inital commit

---
+++ precache-manifest.b7f167e92b2a5616c95fa4802489a163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ec1bb347b693c1a0fdbff8f2b6cfcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8f3eeb1b92a659d407a",
+    "url": "/static/js/2.c06f803d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c06f803d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91599127344a9d0091f",
+    "url": "/static/js/main.e3f4fa8d.chunk.js"
+  },
+  {
+    "revision": "cc57d6e3b4fc5a895001",
+    "url": "/static/js/runtime-main.7081cf4f.js"
+  }
+]);
